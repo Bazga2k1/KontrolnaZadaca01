d9240dba93294449ae48b48fea199e1029909f5a
Bazga2k1 <filippo.bubic@gmail.com>
2023-03-29 19:10:19 +0200

    Update Kontrolna1.js

--- Kontrolna1.js
+++ Kontrolna1.js
@@ -1,16 +1,60 @@
 //1 (3 BODA)
 //Napisi funkciju koja ispisuje parne brojeve od 0 do 100 (ukljucujuci 100)
-let ispisiParne
+function ispisiParne100(){
+    for(let i = 1; i <= 100; i++){
+        if(i % 2){
+            continue;
+        } else{
+            console.log(i);
+        }
+    }
+}
+
+//ispisiParne100();
 
 //2 (3 BODA)
 //Napisi funkciju koja koristi switch selekciju za pretvaranje numerickog unosa u dan u tjednu. Za neispravan unos ispisi prikladnu poruku.
 //INPUT: 1; OUTPUT: "Ponedjeljak";
-//INPUT: 5; OUTPUT: "Petak"; 
+//INPUT: 5; OUTPUT: "Petak";
+function provjeriDan(danUtjednu){
+    switch(danUtjednu){
+        case 1: console.log("Ponedjeljak\n"); break;
+        case 2: console.log("Utorak\n"); break;
+        case 3: console.log("Srijeda\n"); break;
+        case 4: console.log("Četvrtak\n"); break;
+        case 5: console.log("Petak\n"); break;
+        default: console.log("Neispravan unos!\n"); break;
+    }
+}
+
+/*
+let dan = 1;
+provjeriDan(dan);
+*/
 
 //3 (4 BODA)
 //Napisi funkciju koja okrece uneseni string i broji charactere ne uzimajuci u obzir prazna mjesta (eng. whitespaces)
 //INPUT: "Pisanje zadataka nije jednostavan posao";
 //OUTPUT: ("oasop navatsondej ejin akatadaz ejnasiP", 35)
+
+function okreniString(rijec){
+    let count = 0;
+    let novi;
+
+    for(i = rijec; i > 0; i--){
+        if(rijec[i] == " "){
+            continue;
+        } else{
+            novi = novi + rijec[i];
+            count++;
+        }
+    }
+    
+    console.log(novi, ", ", count);
+}
+
+let zadani = "Pisanje zadataka nije jednostavan posao"
+//okreniString(zadani);
 
 //4 (4 BODA)
 //Napisi funkciju koja vraca najkraci kljuc u danom objektu
@@ -29,6 +73,8 @@ const somePeople = {
 //Napisi funkciju u obliku function expressiona koja vraca proste brojeve
 //INPUT: [3, 4, 5, 8, 11]
 //OUTPUT: [3, 5, 11]
+
+
 
 //ZAMJENA ZA 5 (4 BODA)
 //Napisi funkciju u obliku arrow functiona koja kvadrira i vraca dane inpute
